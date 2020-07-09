aedceaf3f954714a573fb77016a084472e08e2ba
Patricio <baptista_patricio@hotmail.com>
2020-07-09 12:11:49 +0100

    test

---
+++ resources/views/home.blade.php
@@ -0,0 +1,231 @@
+
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Raleway&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/34b9ea8fdc.js"></script>
+    <link rel="stylesheet" type = "text/css" href="{{ asset('css/home.css') }}">
+    <link rel = "icon" type = "image/png" href = "{{asset('/storage/logo4.png')}}">
+    <title>mmarketing</title>
+  </head>
+  <body>
+    <header id="header" class="container-fluid">
+      <div class="row navbarbig">
+        <div id="logo" class="col-12 text-center pt-0">
+          <img class="logobig" src="{{asset('/storage/logo4.jpeg')}}" alt="imagen">
+        </div>
+        <div class="col-12 text-center">
+          <nav id="navbar">
+            <ul>
+              <li><a class="navlink black family" href="#home">home</a></li>
+              <li><a class="navlink black family" href="#about">about me</a></li>
+              <li><a class="navlink black family" href="#services">services</a></li>
+              <li><a class="navlink black family" href="#sectors">sectors</a></li>
+              <li><a class="navlink black family" href="#contact">contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div class="row navbarsmall">
+        <div class="col-sm-2">
+          <div id="dropdown-small" class="dropdown">
+           <div onclick="myFunction(this)" class="container1 btn btn-secondary dropdown-toggle pt-3" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         <div class="bar1"></div>
+         <div class="bar2"></div>
+         <div class="bar3"></div>
+       </div>
+           <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+               <a class="dropdown-item small family black" href="/">home</a>
+               <a class="dropdown-item" href="#about">about me</a>
+               <a class="dropdown-item" href="#services">services</a>
+               <a class="dropdown-item" href="#sectors">sectors</a>
+               <a class="dropdown-item" href="#contact">contact</a>
+           </div>
+         </div>
+        </div>
+        <div class="col-sm-10">
+            <img class="logo" src="{{asset('/storage/logo.jpeg')}}" alt="imagen">
+        </div>
+      </div>
+      <script type="text/javascript">
+      function myFunction(x) {
+        x.classList.toggle("change");
+      }
+      </script>
+
+    </header>
+    <main id="main" class="container-fluid pr-0 mr-0">
+      <div class="row palmtrees pr-0 mr-0" style="background-image: url({{asset('/storage/palmtrees.png')}})">
+        <span class="col-12 text-center pt-5 mt-5">
+          <h1 class="title pt-5 family black">Proper marketing</h1>
+          <h2 class="pt-0 subtitle family black">for growing<br>businesses</h2>
+          <button class="contact_button" type="button" name="button"> <a class="white" href="#contact">GET IN TOUCH</a></button>
+        </span>
+      </div>
+      <div id="home" class="row">
+        <div class="col-12 text-center">
+          <p class="medium black family">An independent marketing consultancy<br>for small businesses with tall ambitions</p>
+        </div>
+        <div class="col-12 text-center">
+          <img class="circls" alt="" src="{{asset('/storage/circls.png')}}">
+        </div>
+      </div>
+      <div id="about"class="row">
+        <div class="col-12 text-center">
+          <p class="medium blue family">"I just got to a point where I'd had enough of<br>working for the big guys"</p>
+        </div>
+      </div>
+      <div class="row pt-4">
+        <div class="col-sm-12 col-lg-6 text-center p-0">
+          <img class="marie" src="{{asset('/storage/marie.png')}}" alt="">
+        </div>
+        <div class="col-sm-12 col-lg-6 text-center pr-5 pt-4">
+          <p class="small black family pr-5">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+      <div id="sectors" class="row sectors mt-5 pt-3">
+        <div class="col-12 text-center pt-5">
+          <p class="medium black family">Specialist sectors</p>
+        </div>
+        <div class="sectors col-12 text-center">
+          <p class="small black family">real estate <span class="white">•</span> hospitality <span class="white">•</span> travel <span class="white">•</span> leisure<br>yachting <span class="white">•</span> finance <span class="white">•</span> professional services</p>
+        </div>
+      </div>
+      <div id="services" class="row container pt-3 pb-0 mb-0">
+        <div class="servicesbig col-8 text-left pt-3 pl-5 pb-0 mb-0">
+          <p class="big black family pl-5">How<br>I can<br>help<span class="blue">.</span></p>
+          <img style="position:absolute; bottom:0px;"src="{{asset('/storage/white-palm.png')}}" alt="" class="png2 pb-0 mb-0">
+        </div>
+        <div class="servicessmall col-sm-12 text-left pt-3 pl-5 pb-0 mb-0 pr-0">
+          <p class="big black family pl-5 pr-0">How I can help<span class="blue">.</span></p>
+        </div>
+        <div class="col-sm-12 col-m-4 col-lg-4 text-left pt-5">
+          <article class="services">
+            <h3 class='medium blue family'>Lead generation</h3>
+            <p class="small black family pt-0">Generate enquiries for your product or service</p>
+          </article>
+          <article class="services mt-5">
+            <h3 class='medium blue family'>Database growth</h3>
+            <p class="small black family pt-0">Generate enquiries for your product or service</p>
+          </article>
+          <article class="services mt-5">
+            <h3 class='medium blue family'>Search marketing</h3>
+            <p class="small black family pt-0">Generate enquiries for your product or service</p>
+          </article>
+          <article class="services mt-5">
+            <h3 class='medium blue family'>Social median</h3>
+            <p class="small black family pt-0">Generate enquiries for your product or service</p>
+          </article>
+          <article class="services mt-5">
+            <h3 class='medium blue family'>Email marketing</h3>
+            <p class="small black family pt-0">Generate enquiries for your product or service</p>
+          </article>
+          <article class="services mt-5">
+            <h3 class='medium blue family'>Branding and design</h3>
+            <p class="small black family pt-0">Generate enquiries for your product or service</p>
+          </article>
+          <article class="services mt-5">
+            <h3 class='medium blue family'>Websites</h3>
+            <p class="small black family pt-0">Generate enquiries for your product or service</p>
+          </article>
+          <article class="services mt-5">
+            <h3 class='medium blue family'>Content</h3>
+            <p class="small black family pt-0">Generate enquiries for your product or service</p>
+          </article>
+          <article class="services mt-5">
+            <h3 class='medium blue family'>CRM</h3>
+            <p class="small black family pt-0">Generate enquiries for your product or service</p>
+          </article>
+        </div>
+      </div>
+      <section class="goals">
+      <div class="row container goals pt-3">
+        <div class="goalsbig col-8 text-left pt-3 pl-5">
+          <p class="big white family pl-5">What's<br>your<br>goal<span class="black">?</span></p>
+        </div>
+        <div class="goalssmall col-sm-12 text-center pt-3 pl-5">
+          <p class="big white family pl-5">What's your goal<span class="black">?</span></p>
+        </div>
+        <div class="col-sm-12 col-m-4 col-lg-4 text-left pt-5">
+          <article class="services">
+            <h3 class='medium white family'>Start something new</h3>
+            <p class="small black family pt-0">Generate enquiries for your product or service</p>
+          </article>
+          <article class="services mt-5">
+            <h3 class='medium white family'>Get an overhaul</h3>
+            <p class="small black family pt-0">Generate enquiries for your product or service</p>
+          </article>
+          <article class="services mt-5">
+            <h3 class='medium white family'>Launch a project</h3>
+            <p class="small black family pt-0">Generate enquiries for your product or service</p>
+          </article>
+        </div>
+      </div>
+      </section>
+      <section id="contact" class="contact">
+      <div class="row container pt-3">
+        <div class="contactbig col-sm-12 col-m-8 col-lg-8 text-left pt-3 pl-5">
+          <p class="big black family pl-5">Get<br>in<br>touch<span class="blue">.</span></p>
+        </div>
+        <div class="contactsmall col-sm-12 text-center pt-3 pl-5">
+          <p class="big black family pl-5">Get in touch<span class="blue">.</span></p>
+        </div>
+        <div class="contact-form col-sm-12 col-m-4 col-lg-4 text-left pt-4">
+          <form class="" action="/" method="post">
+            @csrf
+            <div class="row">
+                  <div class="col-sm-12 col-md-6">
+                    <label class="small black family" for="name">Name</label>
+                    <br>
+                    <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"id="name" aria-describedby="nameHelp" required autocomplete="name" value="{{ old('name') }}" autofocus>
+                      @error('name')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
+                  </div>
+                  <div class="col-sm-12 col-md-6">
+                    <label id="labelContact" class="small black family" for="message">e-mail address</label>
+                    <br>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" aria-describedby="emailHelp" required autocomplete="email" value="{{ old('email') }}">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                  </div>
+                  <div class="col-12 mt-2">
+                    <label id="labelContact" class="small black family" for="message">Message</label>
+                    <textarea name="message" rows="5" cols="60" placeholder="Type your message here" class="form-control @error('message') is-invalid @enderror" id="message" aria-describedby="messageHelp" required autocomplete="message" value="{{ old('message') }}"
+                    @error('message')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror></textarea>
+                    <div class="col-12 text-center">
+                      <button class="contact-submit mt-2" type="submit" name="button">Send</button>
+                    </div>
+                  </div>
+                </div>
+          </form>
+        </div>
+      </div>
+      </section>
+</main>
+    <footer class="container-fluid row sectors pt-4 mt-5">
+        <div class="col-6 text-center pt-5">
+          <p class="medium black family">footer</p>
+        </div>
+        <div class="col-6 text-center pt-5">
+          <img class="png" src="{{asset('/storage/white-palm.png')}}" alt="">
+        </div>
+    </footer>
+  <script src="{{ asset('js/home.js') }}"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
