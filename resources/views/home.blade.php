6251b9b298a1d5f62e5a4b9f4669092ea85a3c53
Patricio <baptista_patricio@hotmail.com>
2020-07-09 12:39:45 +0100

    asset2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -220,10 +220,10 @@
           <p class="medium black family">footer</p>
         </div>
         <div class="col-6 text-center pt-5">
-          <img class="png" src="/storage/white-palm.png" alt="">
+          <img class="png" src="{{asset('/storage/white-palm.png')}}" alt="">
         </div>
     </footer>
-  <script src="/js/home.js"></script>
+  <script src="{{ asset('js/home.js') }}"></script>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
